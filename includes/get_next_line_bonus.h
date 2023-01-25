8df4791aa19c9fd625c111197a5159453ae69204
Cleazyy <cleazy2002@gmail.com>
2023-01-25 19:19:33 +0100

    update

--- includes/get_next_line_bonus.h
+++ includes/get_next_line_bonus.h
@@ -6,20 +6,20 @@
 /*   By: fluchten <fluchten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 07:41:39 by fluchten          #+#    #+#             */
-/*   Updated: 2023/01/09 11:31:36 by fluchten         ###   ########.fr       */
+/*   Updated: 2023/01/25 19:15:58 by fluchten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <limits.h>
-
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
 
 char	*get_next_line(int fd);
 int		ft_is_newline(char *stash);
